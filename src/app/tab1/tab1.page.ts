a8f44548baa78336869bd6ffda92b2ad9d3a806f
Tommy Leung <me@supertommy.com>
2020-02-04 11:49:27 -0700

    add basic hello phaser to tab 1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+
+import Phaser from 'phaser'
+import HelloWorldScene from './scenes/HelloWorldScene'
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+	selector: 'app-tab1',
+	templateUrl: 'tab1.page.html',
+	styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page
+{
+	private game: Phaser.Game
 
-  constructor() {}
+	constructor() {}
+
+	ionViewWillEnter()
+	{
+		const config: Phaser.Types.Core.GameConfig = {
+			parent: 'phaser-container-1',
+			type: Phaser.AUTO,
+			width: '100%',
+			height: '100%',
+			physics: {
+				default: 'arcade',
+				arcade: {
+					gravity: { y: 200 }
+				}
+			},
+			scene: HelloWorldScene
+		}
+
+		this.game = new Phaser.Game(config)
+	}
+
+	ionViewDidLeave()
+	{
+		this.game.destroy(true)
+		this.game = null
+	}
 
 }
